b8e41ffcff4680c7b1fc5c0978ad2ea9afe8a9e4
Jonathan Chai <chaijw@usc.edu>
2021-07-18 15:59:20 -0700

    server

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+var dotenv = require('dotenv')
+var dotenvExpand = require('dotenv-expand')
+
+var myEnv = dotenv.config()
+dotenvExpand(myEnv);
+
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+
+mongoose
+    .connect(process.env.MONGO_LOCAL_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
+    .then(() => console.log("Database connected!"))
+    .catch((err) => console.log(err))
+
+const port = 3080;
+const uri = "mongodb://localhost:27017/details";
+
+app.listen(port, function () {
+    console.log("Server is running on Port: " + port);
+});
+
+/*-------------------- Routes ------------------------*/
+
+app.get("/", (req, res) => res.send("Server is up and running"));
+
+var pokemonRoutes = require("./api/pokemonRoutes.js");
+app.use("/pokemon", pokemonRoutes);
